3296c1fc452256638829c5da9a061a2b3a287b55
emmabeynon <emma.beynon@gmail.com>
2015-12-09 10:04:44 +0000

    Step 21: Created test helper sign_in_and_play and refactored tests to use this.

--- app.rb
+++ app.rb
@@ -8,14 +8,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1] = params[:player_1]
-    session[:player_2] = params[:player_2]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = session[:player_1]
-    @player_2 = session[:player_2]
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
   # start the server if ruby file executed directly
